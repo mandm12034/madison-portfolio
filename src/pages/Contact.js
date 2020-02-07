3e09b4fada1a7b47b45ed588ed52ec122c614dc9
Madison Merritt <madisonmerritt@Madisons-MacBook-Air.local>
2020-02-06 21:08:39 -0500

    I changed my contact photo

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import Contact from '../assets/images/Contact.png';
+import Contact2 from '../assets/images/Contact2.png';
 
 const IndexPage = () => (
   <Layout>
     <div id="main">
       <div className="inner">
         <h1>How to contact me</h1>
-        <span className="image main">
-          <img src={Contact} alt="" />
+        <span className="image">
+          <img src={Contact2} alt="" />
         </span>
         <h2>My mailbox is always open for new opportunities.
           <br/>Please feel free to reach out!</h2>
