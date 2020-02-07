c99fea0bbf91552a959cc8677413f11b20a8f89b
Madison Merritt <madisonmerritt@Madisons-MacBook-Air.local>
2020-02-06 21:03:51 -0500

    I made changes to my about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,7 +12,12 @@ const IndexPage = () => (
           <img src={pic1} alt="" />
         </span>
         <p>
-          I love Me.
+          I am currently a senior at Florida State University majoring
+          in Management Information Systems and minoring in Hospitality
+          Management. <br/><br/>
+          I am involved on campus with my sorority, Alpha Gamma Delta. I currently
+          volunteer with my sisters in a variety of charitable partnerships in the Tallahassee
+          community.
         </p>
       </div>
     </div>
