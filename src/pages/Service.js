44b2178ec5f9cab20b16553e5404fb4d8dbf2021
Madison Merritt <madisonmerritt@Madisons-MacBook-Air.local>
2020-02-13 00:03:17 -0500

    I made changes to my service page

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -12,27 +12,31 @@ const IndexPage = () => (
           <img src={pic1} alt="" />
         </span>
         <p>
-          I love service.
+        I have worked in service for almost 5 years now. 
+        I have worked my first job at Dairy Queen to my most 
+        previous job at Subway right on FSU’s campus. My service 
+        jobs have taught me a lot of skills that I would like to 
+        take into my career. I have learned a lot about leadership 
+        from my managers, I have also learned to adapt quickly to 
+        new situations, and I know how to work well under pressure.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
-          rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh
-          porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc
-          ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit
-          sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris,
-          fringilla in aliquam at, euismod in lectus. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. In non lorem sit amet elit placerat maximus. Pellentesque
-          aliquam maximus risus, vel venenatis mauris vehicula hendrerit.
+        My experience in service has also inspired me to look into
+        getting a minor in Hospitality Management. FSU’s college 
+        of Hospitality has the most fun and rewarding classes
+        I will ever take at school. I have taken International
+        Protocol, where I learned about how every country operates
+        and how to interact with them based on their cultures.
+        I also took interesting classes such as Distilled Spirits
+        and International Culture, and Introduction to 
+        Hospitality and Tourism Management, which were really
+        cool classes just to learn more about the hospitality
+        industry overall.
         </p>
         <p>
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent
-          rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo
-          mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget
-          turpis at, elementum convallis elit. Pellentesque enim turpis,
-          hendrerit tristique lorem ipsum dolor.
+        I do love to interact with new people and different
+        cultures, but I would also like to expand my knowledge
+        and love of technology into the hospitality world too.
         </p>
       </div>
     </div>
