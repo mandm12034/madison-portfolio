8f3f6965170cc69880f5fe70b0c175ef026090de
Madison Merritt <madisonmerritt@Madisons-MacBook-Air.local>
2020-02-13 00:16:21 -0500

    I deleted the photo from my management page

--- src/pages/Management.js
+++ src/pages/Management.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import pic1 from '../assets/images/01.jpg';
 
 const IndexPage = () => (
   <Layout>
     <div id="main">
       <div className="inner">
         <h1>Management</h1>
-        <span className="image main">
-          <img src={pic1} alt="" />
-        </span>
         <p>
         Business is the way the world works. So, to me, it is a
         very fun topic to learn about. While in high school,
