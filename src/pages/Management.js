327d210f0a8b084bafcff8f8d67ebb10bb2fae93
Madison Merritt <madisonmerritt@Madisons-MacBook-Air.local>
2020-02-13 00:14:10 -0500

    I made changes to my Management page

--- src/pages/Management.js
+++ src/pages/Management.js
@@ -12,27 +12,26 @@ const IndexPage = () => (
           <img src={pic1} alt="" />
         </span>
         <p>
-          I love Management.
+        Business is the way the world works. So, to me, it is a
+        very fun topic to learn about. While in high school,
+        I decided I would become a business major, and started
+        taking a lot of business classes for dual enrollment. 
+        These were just small introductory classes but they had
+        so much useful information that I still use to this day.
         </p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus
-          rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per
-          conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh
-          porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc
-          ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit
-          sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris,
-          fringilla in aliquam at, euismod in lectus. Pellentesque habitant
-          morbi tristique senectus et netus et malesuada fames ac turpis
-          egestas. In non lorem sit amet elit placerat maximus. Pellentesque
-          aliquam maximus risus, vel venenatis mauris vehicula hendrerit.
+        Now, in college, I started looking at more advanced 
+        management classes - Legal and Ethical Environment of 
+        Business, Financial Management of the Firm, Business 
+        Communications, Organizational Behavior, and Strategic 
+        Management and Business Policy.
         </p>
         <p>
-          Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent
-          rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo
-          mauris, consectetur id ipsum sit amet, fersapien risus, commodo eget
-          turpis at, elementum convallis elit. Pellentesque enim turpis,
-          hendrerit tristique lorem ipsum dolor.
+        All of these classes taught me how to successfully 
+        run a business and how to become a better leader. 
+        I cannot wait to take what I have learned from 
+        these classes and put it into my daily life and 
+        in my future career. 
         </p>
       </div>
     </div>
