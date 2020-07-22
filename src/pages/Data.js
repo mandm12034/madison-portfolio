56c190c3530c2129f40b5b52596670a399e44b03
Madison Merritt <madisonmerritt@Madisons-Air.lan1>
2020-07-22 19:43:02 -0400

    I made changes to my data analysis page

--- src/pages/Data.js
+++ src/pages/Data.js
@@ -9,27 +9,26 @@ const IndexPage = () => (
         <h1>Data Analysis</h1>
         <p>
         While earning my degree in Management Information Systems, 
-        I have been enrolled in classes like MIS analysis and Design 
+        I was enrolled in classes like MIS analysis and Design 
         and Intro to Business Analytics. Data Analysis is always changing 
-        and I learn more and more every day about it.
+        and strive to learn more every day about it.
         </p>
         <p>
-        I am also enrolled in Big Data which helps me learn the ropes of how
+        I was also enrolled in Big Data which helped me learn the ropes of how
          to analyze big data. Since this is only a semester long class, I am only 
         learning the basics of one software – Orange. So far we have covered data 
         cleansing and machine learning, which makes me really excited to learn 
         more about it.
         </p>
         <p>
-        I am also taking Business Intelligence which deals with how to 
-        basically visualize big data. We are using the software Tableau, 
+        I also took Business Intelligence which deals with how to 
+        visualize big data. We used the software, Tableau, 
         which is linked in the “Tableau” tab of my website if you would 
-        like to look at my public account. Basically, we have learned how 
+        like to look at my public account. My class taught me how 
         to join data, and build some basic charts like bar charts and 
-        line charts, but we have also gotten into learning a little bit 
+        line charts, but we also got into learning a little bit 
         more about some more complex visualizations like heat maps and 
-        tree maps as well. I update my Tableau Public account around 
-        every other week.
+        tree maps as well.
         </p>
       </div>
     </div>
